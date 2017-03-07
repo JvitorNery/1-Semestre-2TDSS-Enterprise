d2ebbc7a4961da7dbe5cc54e0936b7d5b8876b56
logonpf <logonpf@fiap.com.br>
2017-03-06 22:57:11 -0300

    JPA DAO Genérico

--- 04-JPA-DesignPatterns/src/br/com/fiap/dao/TimeDAO.java
+++ 04-JPA-DesignPatterns/src/br/com/fiap/dao/TimeDAO.java
@@ -3,5 +3,5 @@ package br.com.fiap.dao;
 import br.com.fiap.entity.Time;
 
 public interface TimeDAO extends GenericDAO<Time, Integer>{
-
+	
 }
--- 04-JPA-DesignPatterns/src/br/com/fiap/dao/impl/GenericDAOImpl.java
+++ 04-JPA-DesignPatterns/src/br/com/fiap/dao/impl/GenericDAOImpl.java
@@ -3,8 +3,10 @@ package br.com.fiap.dao.impl;
 import java.lang.reflect.ParameterizedType;
 import javax.persistence.EntityManager;
 import br.com.fiap.dao.GenericDAO;
+import br.com.fiap.exception.CommitException;
+import br.com.fiap.exception.EntityNotFoundException;
 
-public class GenericDAOImpl<T,K> implements GenericDAO<T, K>{
+public abstract class GenericDAOImpl<T,K> implements GenericDAO<T, K>{
 
 	private EntityManager em;
 	
@@ -19,23 +21,32 @@ public class GenericDAOImpl<T,K> implements GenericDAO<T, K>{
 	
 	@Override
 	public void insert(T entidade) {
-		em.getTransaction().begin();
-		em.persist(entidade);
-		em.getTransaction().commit();
+		em.persist(entidade);		
+	}
+
+	public void save() throws CommitException {
+		try{
+			em.getTransaction().begin();
+			em.getTransaction().commit();
+		}catch(Exception e){
+			if (em.getTransaction().isActive()) //valida se existe uma transação ativa
+				em.getTransaction().rollback();
+			e.printStackTrace();
+			throw new CommitException("Erro ao realizar o commit");
+		}
 	}
 
 	@Override
 	public void update(T entidade) {
-		em.getTransaction().begin();
 		em.merge(entidade);
-		em.getTransaction().commit();
 	}
 
 	@Override
-	public void delete(K id) {
-		em.getTransaction().begin();
-		em.remove(findById(id));
-		em.getTransaction().commit();
+	public void delete(K id) throws EntityNotFoundException {
+		T entidade = findById(id);
+		if (entidade == null)
+			throw new EntityNotFoundException("Entidade não existe");
+		em.remove(entidade);
 	}
 
 	@Override
