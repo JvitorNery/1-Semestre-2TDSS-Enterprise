507d19b77c1c9ebe475090738a3a03d90694889e
logonpf <logonpf@fiap.com.br>
2017-04-19 20:50:37 -0300

    Mapeamento Avançado - Chave Composta

--- 07-JPA-JPQL-Apostila/src/br/com/fiap/dao/impl/ReservaDAOImpl.java
+++ 07-JPA-JPQL-Apostila/src/br/com/fiap/dao/impl/ReservaDAOImpl.java
@@ -1,5 +1,7 @@
 package br.com.fiap.dao.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.fiap.dao.ReservaDAO;
@@ -9,6 +11,15 @@ public class ReservaDAOImpl extends GenericDAOImpl<Reserva,Integer> implements R
 
 	public ReservaDAOImpl(EntityManager entityManager) {
 		super(entityManager);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<Reserva> buscarPorNumeroDias(int dias) {
+		return em.createNativeQuery("SELECT * FROM RESERVA WHERE "
+				+ "NUMERO_DIAS = :d",Reserva.class)
+				.setParameter("d", dias)
+				.getResultList();
 	}
 
 }
